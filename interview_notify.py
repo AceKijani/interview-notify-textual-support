19f3dd09ae6cf9a2739ea5f1b7620f0b9efd6e63
Gavin <gpaull@email.sc.edu>
2024-07-08 12:30:13 -0400

    Changed the way that logfiles are discovered if --textual arg is passed which allows for discovery of all logfiles in subdirectiories that are created by the textual irc.

--- interview_notify.py
+++ interview_notify.py
@@ -79,10 +79,13 @@ def log_parse(log_path, parser_stop):
         if check_trigger(line, 'Currently interviewing: {}'.format(args.nick)):
             logging.info('YOUR INTERVIEW IS HAPPENING ❗')
             notify(line, title='Your interview is happening❗', tags='rotating_light', priority=5)
-        elif check_trigger(line, 'Currently interviewing:'):
+        elif check_trigger(line, 'interviewer'):
+            logging.info('YOUR INTERVIEW IS HAPPENING ❗')
+            notify(line, title='Your interview is happening❗', tags='rotating_light', priority=5)
+        elif check_trigger(line, trigger='Currently interviewing:'):
             logging.info('interview detected ⚠️')
             notify(line, title='Interview detected', tags='warning')
-        elif check_trigger(line, '{}:'.format(args.nick), disregard_bot_nicks=True):
+        elif check_trigger(line, '{}:'.format(args.nick), disregard_bot_nicks=True) or check_trigger(line, '{}'.format(args.nick), disregard_bot_nicks=True):
             logging.info('mention detected ⚠️')
             notify(line, title="You've been mentioned", tags='wave')
         elif check_words(line, triggers=['quit', 'disconnect', 'part', 'left', 'leave']):
